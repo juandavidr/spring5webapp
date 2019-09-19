c15d5e5cbdd00253ce8d7b71c706c2d41374681b
Juan David Rodriguez Romero <jd.rodriguez@globant.com>
2019-09-19 12:01:01 -0500

    Created the simple book controllers

---
+++ src/main/java/co/syalar/spring5webapp/controllers/BookController.java
@@ -0,0 +1,25 @@
+package co.syalar.spring5webapp.controllers;
+
+import co.syalar.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by jd.rodriguez
+ */
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
